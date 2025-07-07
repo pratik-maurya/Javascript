a61aa532e1b601f3148b27aaa34cac08cbc2fde8
pratik-maurya <pratikmauryastudy@gmail.com>
2025-07-07 22:04:47 +0530

    finad even and odd numbers

---
+++ even_odd.js
@@ -0,0 +1,11 @@
+function even_odd(number) {
+  if(number %2 == 0){
+    console.log("The number is even.");
+  }else{
+    console.log("The number is odd.");
+  }
+}
+even_odd(10);
+even_odd(11);
+even_odd(-11);
+even_odd(-2);
