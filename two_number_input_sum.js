ac370ed46cd55d694cd3ecd62138c36ecb4388e1
pratik-maurya <pratikmauryastudy@gmail.com>
2025-07-07 22:04:14 +0530

    Sum of two numbers

---
+++ two_number_input_sum.js
@@ -0,0 +1,16 @@
+// let a = 5;
+// let b = 3;
+// let sum = a + b;
+// console.log("The sum of the two numbers is: " + sum);
+
+// =========================or =========================
+
+function sum(a, b) {
+    let sum = a + b;
+    console.log("The sum of the two numbers is: " + sum);
+}
+sum(0,1.5);
+sum(0,0);
+sum(135,986);
+
+// =========================or =========================
