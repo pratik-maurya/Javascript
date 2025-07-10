47bf12153cc0f7f36a121f3d8c4f47b6cab1af55
pratiktml <pratikm.vspl@tatamotors.com>
2025-07-10 17:09:47 +0530

    reverse of string

---
+++ reverse_of_string.js
@@ -0,0 +1,12 @@
+// function reverseOfString(a){
+//     let value = a.split('').reverse().join('')
+//     console.log(value)
+// }
+// reverseOfString('Ashutosh')
+
+function reverseOfString(a){
+    for(let i = 0; i<a.length; i++){
+        
+    }
+}
+reverseOfString('ashish')
